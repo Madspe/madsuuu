5c57814c44c76357913c08356304ac2d9d5fa322
Mads Pettersen <madsp@uia.no>
2022-10-02 13:57:01 +0200

    ny versjon, fungerende databasekommunikasjon lokalt

---
+++ bacit-dotnet.MVC/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using bacit_dotnet.MVC.Entities;
+using bacit_dotnet.MVC.Models.Users;
+
+namespace bacit_dotnet.MVC.Repositories
+{
+    public interface IUserRepository
+    {
+        void Save(UserEntity user);
+        List<UserEntity> GetUsers();
+        void Delete(string email);
+
+        void Edit(UserEntity user);
+        
+    }
+
+
+}
